8d44be84ed55947b681b10e777585b0e4a9428e2
lucas-de-souza-pereira <lucas.de-souza-pereira@laplateforme.io>
2025-07-02 13:41:08 +0200

    add database connexion

---
+++ src/main/java/com/example/DAO/Database.java
@@ -0,0 +1,18 @@
+package com.example.DAO;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import io.github.cdimascio.dotenv.Dotenv;
+
+public class Database {
+    private static final Dotenv dotenv = Dotenv.load();
+
+    private static final String URL = dotenv.get("DB_URL");
+    private static final String USER = dotenv.get("DB_USER");
+    private static final String PASSWORD = dotenv.get("DB_PASSWORD");
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
